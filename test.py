b0eaa60c5b42e5ce26a713da1dd476dcfcfdc524
siddhu.classwork <siddhu.classwork@gmail.com>
2017-01-11 08:41:48 -0500

    Changing Stuff

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print("Hello World")
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+coding_dna = Seq("ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG", IUPAC.unambiguous_dna)
+protein = coding_dna.translate()
+print(protein)
