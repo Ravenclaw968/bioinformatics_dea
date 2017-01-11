8889d1c95d1bd95f2a5f3f69a49f7f8d858bc96e
siddhu.classwork <siddhu.classwork@gmail.com>
2017-01-11 08:47:34 -0500

    New Commit

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
 from Bio.Seq import Seq
-from Bio.Alphabet import IUPAC
-coding_dna = Seq("ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG", IUPAC.unambiguous_dna)
-protein = coding_dna.translate()
-print(protein)
+print("AAAA".count("AA"))
